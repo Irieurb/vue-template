146d545b28516b72089a2b9e6cb784c09a3dd67b
simple-tools <info@simple-tools.de>
2021-11-02 16:44:22 +0100

    new init commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+      </div>
+    </v-app-bar>
+    <v-main>
+        <h1 class="text-center">Hallo Vue</h1>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
