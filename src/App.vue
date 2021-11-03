ca54d670cfe8fa16aa3a811156d4f2a93b3480c2
Simple Tools <info@simple-tools.de>
2021-11-03 16:50:42 +0100

    Todo Liste finalisiert

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,22 @@
       </div>
     </v-app-bar>
     <v-main>
-        <h1 class="text-center">Hallo Vue</h1>
+        <h1 class="text-center ma-6">Todo Liste</h1>
+        <div class="row justify-center">
+          <div v-for="(item, index) in todo" class="card col-3 ma-10" style="width: 250px;">
+            <img class="card-img-top" :src="item.pic" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">{{ item.title }}</h5>
+              <p class="card-text">{{ item.detail }}</p>
+              <hr>
+              <span v-if="item.done" class="badge badge-success">Erledigt</span>
+              <span v-else class="badge badge-warning">Offen</span>
+              <hr>
+              <a v-if="item.done" href="#" @click="toggleItem(index)" class="card-link">Neu Öffnen</a>
+              <a v-else href="#" @click="toggleItem(index)" class="card-link">Erledigt</a>
+            </div>
+        </div>
+      </div>
     </v-main>
   </v-app>
 </template>
@@ -20,10 +35,65 @@
 <script>
 
 export default {
+
   name: 'App',
 
   data: () => ({
     //
+    todo: [
+      {
+        id: 1,
+        done: false,
+        title: "Titel",
+        detail: "Hier steht ein Detailtext",
+        pic: "https://picsum.photos/250/100"
+      },
+      {
+        id: 1,
+        done: false,
+        title: "Titel",
+        detail: "Hier steht ein Detailtext",
+        pic: "https://picsum.photos/250/100"
+      },
+      {
+        id: 1,
+        done: true,
+        title: "Titel",
+        detail: "Hier steht ein Detailtext",
+        pic: "https://picsum.photos/250/100"
+      },
+      {
+        id: 1,
+        done: false,
+        title: "Titel",
+        detail: "Hier steht ein Detailtext",
+        pic: "https://picsum.photos/250/100"
+      },
+      {
+        id: 1,
+        done: true,
+        title: "Titel",
+        detail: "Hier steht ein Detailtext",
+        pic: "https://picsum.photos/250/100"
+      },
+      {
+        id: 1,
+        done: false,
+        title: "Titel",
+        detail: "Hier steht ein Detailtext",
+        pic: "https://picsum.photos/250/100"
+      }
+    ]
   }),
+
+  methods: {
+
+    toggleItem: function (index) {
+
+      this.todo[index].done = !this.todo[index].done
+
+    }
+  }
+
 };
 </script>
---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <img class="card-img-top" :src="item.pic" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title">{{ item.title }}</h5>
+      <p class="card-text">{{ item.detail }}</p>
+      <hr>
+      <span v-if="item.done" class="badge badge-success">Erledigt</span>
+      <span v-else class="badge badge-warning">Offen</span>
+      <hr>
+      <a href="#" @click="toggleItem(index)" class="card-link">{{  }}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Card",
+
+  props: {
+    item: Object,
+    default: {}
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
